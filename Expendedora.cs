73cc22054838baf2a2a419df9c71cd622f804a5f
Alumno <Alumno@L17-2543301>
2023-08-23 11:48:49 -0600

    Agregar archivos de proyecto.

---
+++ Expendedora.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading;
+
+
+namespace ExpendedoraG2_2024_1
+{
+    internal class Expendedora
+    {
+        #region Atributos
+        private string marca;
+        private ushort cantproductos;
+        private float precio;
+        #endregion
+
+        #region Métodos
+        private void Saludar()
+        {
+            Console.WriteLine("Bienvendo, elige un producto");
+        }
+        private void LimpiarDisplay()
+        {
+            Thread.Sleep(2000);
+            Console.Clear();
+        }
+
+        //Regresa el código
+        public string MostrarProducto()
+        {
+            string codigo = "";
+            Console.WriteLine("3A: Doritos \n 3B: Churrumais");
+            Console.WriteLine("Ingresa el código del producto");
+            codigo = Console.ReadLine();
+            return codigo;
+            //string codigo = Console.ReadLine();
+            //return codigo;
+
+        }
+
+        public void MostrarPrecio(string codigo)
+        {
+            switch (codigo)
+            {
+                case "3A":
+                    Console.WriteLine("Precio:{0}", precio);
+                    break;
+                case "3B":
+                    Console.WriteLine("Precio:{0}", precio-6);
+                    break;
+                default:
+                    Console.WriteLine("No ingresaste un producto válido");
+                    break;
+
+
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public Expendedora()
+        {
+            marca = "AMS";
+            precio = 18;
+            Saludar();
+            LimpiarDisplay();
+            Console.WriteLine("Marca: {0}",marca);
+            LimpiarDisplay();
+            string codigo= MostrarProducto();
+            LimpiarDisplay();
+            MostrarPrecio(codigo);
+
+
+        }
+        #endregion
+    }
+}
